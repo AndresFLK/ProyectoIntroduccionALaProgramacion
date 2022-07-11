c017f1d33b347d67f337bf6b3657440711e0b246
fransalazar28 <107603097+fransalazar28@users.noreply.github.com>
2022-07-10 23:17:48 -0600

    Add files via upload

---
+++ funcionarios.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sc202.parteproyecto;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Francisco
+ */
+public class funcionarios {
+    
+}
+     public static void main(String[] args) 
+    {
+        String nombre = JOptionPane.showInputDialog("Ingrese el pago correspondiente  ");
+        String apellido = JOptionPane.showInputDialog("Digite su ID de funcionario ");
+        String cedula = JOptionPane.showInputDialog("Digite su nombre");
+        String correo = JOptionPane.showInputDialog("Digite el nombre de la institucion");
+        
+    
+}
